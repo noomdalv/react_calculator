719621dc2ca18d391d353e7f29b65a00968f71de
noomdalv <vladlunasan@gmail.com>
2020-02-26 11:30:38 -0500

    start heroku with create-react-app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = () => <h1>Hello World</h1>;
+const Hello = () => <h1>Hello World, Milestone 1 complete</h1>;
 
 ReactDOM.render(
   <Hello />,
