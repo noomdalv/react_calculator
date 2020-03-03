19bd32ac2cc3e816a7e347f51e3f543bbd8f75aa
noomdalv <vladlunasan@gmail.com>
2020-03-03 12:40:40 -0500

    implemented handleClick for App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-import calculate from '../logic/calculate'; // eslint-disable-line no-unused-vars
+import { calculate } from '../logic/calculate'; // eslint-disable-line no-unused-vars
 
-const App = () => (
-  <div id="app">
-    <Display />
-    <ButtonPanel />
-  </div>
-);
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			total: null,
+			next: null,
+			operation: null
+		}
+	}
+
+	handleClick(buttonName) {
+		let data = operate(calculate, buttonName);
+
+		this.setState({
+			total: data.total,
+			next: data.next,
+			operation: data.operation
+		})
+	}
+
+	render(){
+		return (
+			<div id="app">
+		    <Display result={this.state.total}/>
+		    <ButtonPanel clickHandler={handleClick} />
+		  </div>
+		)
+	}
+};
+
 
 export default App;
